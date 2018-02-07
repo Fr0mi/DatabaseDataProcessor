b4a6ec18bb0831b6c214f288497af94d2c2ab901
nigr <nigr@NIGR-W380>
2018-02-07 11:22:00 +0100

    DbConnector added

--- DatabaseDataProcessor/Class1.cs
+++ DatabaseDataProcessor/Class1.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DatabaseDataProcessor
 {
-    public class Class1
+    public class DbConnector
     {
-    }
+        private const string ServerName = "test";
+        private const string DatabaseName = "test";
+        private const string MyUsername = "test";
+        private const string MyPassword = "test";
+
+        public bool EstablishConnection()
+        {
+            using (var conn = new SqlConnection())
+            {
+                
+                conn.ConnectionString = $"Server={ServerName}; Database={DatabaseName}; Uid={MyUsername}; Pwd={MyPassword}";
+            }
+
+            return false;
+        }
+    }   
+
 }
